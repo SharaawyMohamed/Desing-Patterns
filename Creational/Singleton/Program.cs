f219ff18a2ea5d861cb7a71f354cb8d0e7981db1
SharaawyMohamed <sharawem585@gmail.com>
2024-08-05 10:18:29 +0300

    First Create Adapter Pattern

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using Singleton.Start;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static MemoryLogger logger;
+        static void Main(string[] args)
+        {
+            AssignVoucher("sharawy275@gmail.com", "ABC123");
+
+            UserVoucher("ABC123");
+            logger.ShowLog();
+            Console.ReadKey();
+        }
+        static void AssignVoucher(string email,string voucher)
+        {
+            logger = MemoryLogger.Instance;
+            // logic here
+            logger.LogInfo($"Voucher {voucher} assigned");
+            // logic here
+            logger.LogError($"unable to send email {email}");
+        }
+        static void UserVoucher(string voucher)
+        {
+            logger = MemoryLogger.Instance;
+
+            // logic here
+            logger.LogWarning($"3 attempts made to validate the voucher");
+            // logic here
+            logger.LogInfo($"{voucher} is used");
+        }
+    }
+}
---
+++ Creational/Singleton/Start/LogType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Start
+{
+    internal enum LogType
+    {
+        INFO,
+        WARNIN,
+        ERROR
+    }
+
+}
